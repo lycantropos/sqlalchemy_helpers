e54ddcc2686831cdfd6e35fa9fb540a104dba9be
ace <azatibrakov@gmail.com>
2017-05-02 14:19:26 +0700

    Bump version: 0.0.0 → 0.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='sqlalchemy_helpers',
-      version='0.0.0',
+      version='0.0.1',
       description='Helper functions for comfortable working with SQLAlchemy.',
       author='Azat Ibrakov',
       author_email='azatibrakov@gmail.com',
