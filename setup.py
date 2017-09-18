32978cad9da39d39e77a8c99521c25375414cd58
Azat Ibrakov <azatibrakov@gmail.com>
2017-09-19 00:51:40 +0700

    Added 'MAX_IDENTIFIERS_LENGTH'

--- setup.py
+++ setup.py
@@ -1,14 +1,32 @@
 from setuptools import (setup,
                         find_packages)
 
-setup(name='sqlalchemy_helpers',
-      packages=find_packages(),
-      version='0.0.3',
-      description='Helper functions for comfortable working with SQLAlchemy.',
+import sqlalchemy_helpers
+from sqlalchemy_helpers.config import PROJECT_NAME
+
+project_base_url = 'https://github.com/lycantropos/sqlalchemy_helpers/'
+setup(name=PROJECT_NAME,
+      version=sqlalchemy_helpers.__version__,
       author='Azat Ibrakov',
       author_email='azatibrakov@gmail.com',
-      url='https://github.com/lycantropos/sqlalchemy_helpers',
-      download_url='https://github.com/lycantropos/sqlalchemy_helpers/archive/'
-                   'master.tar.gz',
+      url=project_base_url,
+      download_url=project_base_url + 'archive/master.tar.gz',
+      description=sqlalchemy_helpers.__doc__,
+      long_description=open('README.rst').read(),
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Operating System :: POSIX',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Topic :: Database :: Database Engines/Servers',
+      ],
+      packages=find_packages(),
       keywords=['SQLAlchemy'],
       install_requires=['sqlalchemy>=1.1.6'])
--- sqlalchemy_helpers/config.py
+++ sqlalchemy_helpers/config.py
@@ -1,1 +1,8 @@
 PROJECT_NAME = 'sqlalchemy_helpers'
+
+# more info at:
+# for PostgreSQL
+# https://www.postgresql.org/docs/current/static/sql-syntax-lexical.html#SQL-SYNTAX-IDENTIFIERS
+# for MySQL
+# https://dev.mysql.com/doc/refman/5.5/en/identifiers.html
+MAX_IDENTIFIER_LENGTH = 63
