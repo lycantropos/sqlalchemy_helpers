e12f368b8b22d3f2955b9a51a99d0e9dc697bafa
Azat Ibrakov <azatibrakov@gmail.com>
2017-09-19 02:12:44 +0700

    Added checks for context managers and non-empty strings

--- tests/utils.py
+++ tests/utils.py
@@ -13,3 +13,20 @@ def example(strategy: SearchStrategy) -> Any:
                                   max_iterations=10000,
                                   database=None,
                                   verbosity=Verbosity.quiet))
+
+
+def is_context_manager(object_: Any) -> bool:
+    return (has_callable_attribute(object_,
+                                   attribute='__enter__')
+            and has_callable_attribute(object_,
+                                       attribute='__exit__'))
+
+
+def has_callable_attribute(object_: Any,
+                           *,
+                           attribute: str) -> bool:
+    return callable(getattr(object_, attribute, None))
+
+
+def is_non_empty_string(string: str) -> bool:
+    return isinstance(string, str) and string
